5c2876a5be5f15539a28bd10a3d3678f375e5263
JamBool <mailforgit2023@gmail.com>
2024-05-06 11:15:55 +0300

    В AppDelegate инициализирован NSPersistentContainer

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -6,17 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-      
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 }
 
