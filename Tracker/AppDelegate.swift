a48265c0484f4f1da977a9b4a81b3f6a431f2b64
JamBool <mailforgit2023@gmail.com>
2024-05-06 11:06:38 +0300

    Создан новый файл с типом *.xcdatamodeld, в нём определены сущности и их атрибуты.

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow()
-        window?.rootViewController = MainTabBarController()
-        window?.makeKeyAndVisible()
+//        window = UIWindow()
+//        window?.rootViewController = MainTabBarController()
+//        window?.makeKeyAndVisible()
         return true
     }
     
