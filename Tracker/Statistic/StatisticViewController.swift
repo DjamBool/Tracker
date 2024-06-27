40b156e0bace879d609f4b84257dcbe4f0b4ef4a
JamBool <mailforgit2023@gmail.com>
2024-06-27 13:43:52 +0300

    изменения в экране статистики

--- Tracker/Statistic/StatisticViewController.swift
+++ Tracker/Statistic/StatisticViewController.swift
@@ -1,116 +1,78 @@
 
 import UIKit
 
-protocol StatisticViewControllerDelegate: AnyObject {
-    func updateStatistics()
-}
-
-class StatisticViewController: UIViewController {
-
+final class StatisticViewController: UIViewController {
+    
     private let trackerRecordStore = TrackerRecordStore()
-    private var completedTrackers: [TrackerRecord] = []
-    weak var delegate: StatisticViewControllerDelegate?
-
+    private var completedTrackers: Set<TrackerRecord> = []
+    private var trackers = [Tracker]()
+    
     private lazy var emptyView: StatisticsNotFoundView = {
         let view = StatisticsNotFoundView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    private lazy var containerView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 15
-        return view
-    }()
-    
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .ypBlack
-        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        label.textAlignment = .left
-        return label
-    }()
-    
-    private lazy var subtitleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .ypBlack
-        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-        label.textAlignment = .left
-        return label
-    }()
-    
-    private let gradientLayer: CAGradientLayer = {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [
-            UIColor.colorSelection1.cgColor,
-            UIColor.colorSelection3.cgColor,
-            UIColor.colorSelection5.cgColor
-        ]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
-        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
-        return gradientLayer
-    }()
+    private let statisticsView = StatisticsView(title: "0", subtitle: "Трекеров завершено")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Статистика"
         setupViews()
+        updateStatistics()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateStatistics()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        statisticsView.frame = CGRect(x: 16, y: self.view.frame.midY - 45, width: self.view.frame.width - 32, height: 90)
     }
     
     func setupViews() {
         view.backgroundColor = UIColor.systemBackground
-        [titleLabel, subtitleLabel].forEach { containerView.addSubview($0) }
-        [emptyView, containerView].forEach { view.addSubview($0) }
-            
-        containerView.layer.insertSublayer(gradientLayer, at: 0)
+        [emptyView, statisticsView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        statisticsView.frame = CGRect(x: 16, y: self.view.frame.midY, width: self.view.frame.width - 32, height: 90)
+        statisticsView.setupUI()
         
-            NSLayoutConstraint.activate([
-                
-                emptyView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                emptyView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                emptyView.topAnchor.constraint(equalTo: view.topAnchor),
-                emptyView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                
-                containerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 206),
-                containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-                containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-                containerView.heightAnchor.constraint(equalToConstant: 90),
-                
-                titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 12),
-                titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
-                titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
-                titleLabel.heightAnchor.constraint(equalToConstant: 41),
-                
-                subtitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
-                subtitleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-                subtitleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -12),
-                subtitleLabel.heightAnchor.constraint(equalToConstant: 18),
-            ])
-        }
-    
-    private func updateTrackerRecords() {
-            completedTrackers = trackerRecordStore.trackerRecords
-            titleLabel.text = "\(completedTrackers.count)"
-            subtitleLabel.text = String.localizedStringWithFormat(NSLocalizedString("trackersCompleted", comment: "число дней"), completedTrackers.count)
-        emptyView.isHidden = completedTrackers.count > 0
-            containerView.isHidden = completedTrackers.count == 0
-            delegate?.updateStatistics()
-            trackerRecordStore.update()
-            updateStatistics()
-        }
-    
+        NSLayoutConstraint.activate([
+            
+            emptyView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyView.topAnchor.constraint(equalTo: view.topAnchor),
+            emptyView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            statisticsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            statisticsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            statisticsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            statisticsView.heightAnchor.constraint(equalToConstant: 90)
+        ])
     }
-   
-
-extension StatisticViewController: StatisticViewControllerDelegate {
-    func updateStatistics() {
-        DispatchQueue.main.async{
-            self.updateTrackerRecords()
+    
+    private func updateStatistics() {
+        do {
+            completedTrackers = try trackerRecordStore.fetchRecords()
+            statisticsView.configValue(value: completedTrackers.count)
+            updateUI()
+        } catch {
+            print("Ошибка при получении записей: \(error)")
+        }
+    }
+    
+    private func updateUI() {
+        if completedTrackers.isEmpty {
+            emptyView.isHidden = false
+            statisticsView.isHidden = true
+        } else {
+            emptyView.isHidden = true
+            statisticsView.isHidden = false
         }
     }
 }
+
 
